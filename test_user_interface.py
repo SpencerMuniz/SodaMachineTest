41ca80867419084a28eb8ee990fcad5c5d8892c6
SpencerMuniz <sbottomley44@gmail.com>
2021-10-13 14:22:59 -0700

    finished display payment value

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,4 +1,5 @@
 import unittest
+from coins import Dime, Nickel, Penny, Quarter
 import user_interface
 from cans import Cola, OrangeSoda, RootBeer
 
@@ -6,27 +7,27 @@ from cans import Cola, OrangeSoda, RootBeer
 class TestValidateMainMenu(unittest.TestCase):
     def test_validate_main_menu(self):
         answer_one = user_interface.validate_main_menu(1)
-        self.assertEquals(answer_one, (True, 1))
-    def test_validate_main_menu(self):
+        self.assertEqual(answer_one, (True, 1))
+    def test_validate_main_menu_two(self):
         answer_two = user_interface.validate_main_menu(2)
-        self.assertEquals(answer_two, (True, 2))
-    def test_validate_main_menu(self):
+        self.assertEqual(answer_two, (True, 2))
+    def test_validate_main_menu_three(self):
         answer_three = user_interface.validate_main_menu(3)
-        self.assertEquals(answer_three, (True, 3))
-    def test_validate_main_menu(self):
+        self.assertEqual(answer_three, (True, 3))
+    def test_validate_main_menu_four(self):
         answer_four = user_interface.validate_main_menu(4)
-        self.assertEquals(answer_four, (True, 4))
-    def test_validate_main_menu(self):
+        self.assertEqual(answer_four, (True, 4))
+    def test_validate_main_menu_five(self):
         answer_five = user_interface.validate_main_menu(5)
         self.assertEqual(answer_five, (False, None))
 
 class TestTryParseInt(unittest.TestCase):
     def test_try_parse_int(self):
         answer = user_interface.try_parse_int("10")
-        print(answer)
-    def test_try_parse_int(self):
+        self.assertEqual(answer, 10)
+    def test_try_parse_int_word(self):
         answer = user_interface.try_parse_int("hello")
-        print(answer)
+        self.assertEqual(answer, 0)
 
 class TestGetUniqueCanNames(unittest.TestCase):
     def test_get_unique_can_names(self):
@@ -34,9 +35,28 @@ class TestGetUniqueCanNames(unittest.TestCase):
         root_beer = RootBeer()
         orange_soda = OrangeSoda()
         list_of_soda = [cola, cola, orange_soda, orange_soda, root_beer, root_beer]
-        print(user_interface.get_unique_can_names(list_of_soda))
+        unique_cans = user_interface.get_unique_can_names(list_of_soda)
+        self.assertEqual(len(unique_cans), 3)
 
+    def test_get_unique_can_names_no_cans(self):
+        list_of_soda = []
+        unique_cans = user_interface.get_unique_can_names(list_of_soda)
+        self.assertEqual(len(unique_cans), 0)
 
+class TestDisplayPaymentValue(unittest.TestCase):
+    def test_display_payment_value(self):
+        penny = Penny()
+        nickel = Nickel()
+        dime = Dime()
+        quarter = Quarter()
+        list_of_money = [penny, nickel, dime, quarter]
+        total_value = user_interface.display_payment_value(list_of_money)
+        self.assertEqual(total_value, .41) 
+
+    def test_display_payment_value_of_nothing(self):
+        list_of_money = []
+        total_value = user_interface.display_payment_value(list_of_money)
+        self.assertEqual(total_value, 0) 
 
 if __name__ == '__main__':
     unittest.main()
